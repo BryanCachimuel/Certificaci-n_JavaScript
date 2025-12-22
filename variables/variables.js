030e12a2e511bb8ce02c8de4e4c884b5fe102a3f
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-21 22:49:48 -0500

    Aplicando las reglas que se deben usar para crear variables

--- variables/variables.js
+++ variables/variables.js
@@ -1,2 +1,11 @@
-var miNumero = 15;
-console.log(miNumero);
+// Reglas de nombres de las variables
+
+// Puede iniciar una variable: letras, $, _, posteriormente se pueden usar números
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
