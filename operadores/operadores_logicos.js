6156e90453ce522c8f05ef04a3282a0acbddebdc
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-27 22:49:20 -0500

    Aprendiendo a usar el operador lógico and o Y

---
+++ operadores/operadores_logicos.js
@@ -0,0 +1,7 @@
+/* Operadores Lógicos */
+let a = true;
+let b = false;
+
+// Operador && (and o Y)
+// Regresa verdadero si ambos operandos son verdaderos
+console.log(`${a} && ${b} -> ${a && b}`);
