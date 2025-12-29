e0cf8afd1b74cc188dd4df0ca65c0f59bd9f816c
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-28 22:14:18 -0500

    Agregando el bloque else para poder validar otro tipo de resultado que se puede dar del analisis propuesto en el ejercicio

--- sentencias/sentencias.js
+++ sentencias/sentencias.js
@@ -1,7 +1,10 @@
 /* Sentencia if */
-let miNumero = 10;
+let miNumero = -1;
 
 // revisar si el número es positivo
 if(miNumero > 0) {
     console.log(`${miNumero}: es positivo`);
+}
+else {
+    console.log(`${miNumero} no es positivo`);
 }
