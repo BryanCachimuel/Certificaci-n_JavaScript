3d863f7d843be87a820483bdb23834afc0e6b47f
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-28 22:44:14 -0500

    Realizando el ejercicio de los días de la semana con las sentencias if, else if y else

--- sentencias/ejercicios.js
+++ sentencias/ejercicios.js
@@ -11,3 +11,33 @@ else if(edadPersona <= 17 && edadPersona >= 1) {
 else {
     console.log(`${edadPersona}, lo que as ingresado no es una edad`)
 }
+
+let diaSemana = 5;
+
+if(diaSemana === 1) {
+   console.log(`${diaSemana}: Lunes `); 
+}
+else if(diaSemana === 2) {
+    console.log(`${diaSemana}: Martes `);
+}
+else if(diaSemana === 3) {
+    console.log(`${diaSemana}: Miercoles `);
+}
+else if(diaSemana === 4) {
+    console.log(`${diaSemana}: Jueves `);
+}
+else if(diaSemana === 5) {
+    console.log(`${diaSemana}: Viernes `);
+}
+else if(diaSemana === 6) {
+    console.log(`${diaSemana}: Sábado `);
+}
+else if(diaSemana === 7) {
+    console.log(`${diaSemana}: Domingo `);
+}
+else if(diaSemana > 7) {
+    console.log(`${diaSemana}: número ingresado no valido `);
+}
+else if(diaSemana < 1) {
+    console.log(`${diaSemana}: número ingresado no valido `);
+}
