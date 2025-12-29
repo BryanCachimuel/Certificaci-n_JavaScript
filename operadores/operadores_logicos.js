0ed8db856c155f7df9ae8271c311c51bf13c51c2
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-28 22:04:41 -0500

    Aplicando el proceso de la precedencia de operadores en un ejercicio

--- operadores/operadores_logicos.js
+++ operadores/operadores_logicos.js
@@ -26,3 +26,25 @@ let dato = 3;
 // revisamos si está dentro del rango
 let dentroRango = dato >= minimo && dato <= maximo;
 console.log('Valor dentro del rango: ', dentroRango);
+
+/* Precedencia de Operadores
+    1. Parentesis y corchetes
+    2. Operadores unarios, como -, ++, --, !
+    3. Aritméticos *, / y %
+    4. Aritméticos + y -
+    5. Relacionales <, <=, > y >=
+    6. Igualdad == y !=
+    7. Lógicos && y ||
+    8. Asignación =, +=, -=, *=, etc
+*/
+
+// Ejercicio. Se revisa de izquierda a derecha
+let c = 12 / 3 + 2 * 3 -1;
+
+// Paso 1. División 12 / 3 = 4
+// Paso 2. Multiplicaciób 2 * 3 = 6
+// Paso 3. Suma 4 + 6 = 10
+// Paso 4. Resta 10 - 1 = 9 
+
+console.log(c);
+
