4943c461d95132e630fd4d0ff40f3a0742e5a87e
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-02 22:47:09 -0500

    Realizando el ejercicio del ciclo for para imprimir los primeros negativos de 10

--- ciclos/ejercicios.js
+++ ciclos/ejercicios.js
@@ -1,5 +1,9 @@
 /* Imprimir los primeros 10 números de 3 en 3 */
-let maximo = 10, minimo = 10;
-for(let i = 1; i <= maximo; i+=3){
+let maximo = 10, minimo = -10;
+for(let i = 1; i <= maximo; i += 3){
     console.log(i)
+}
+
+for(let j = 1; j >= minimo; j -= 3) {
+    console.log(j)
 }
