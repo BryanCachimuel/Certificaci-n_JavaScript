8220a4510ffffb22b5fd6ea108a173425aec824e
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-04 22:49:46 -0500

    Aprendiendo a simplificar la sintaxis de un arreglo

---
+++ arreglos/arreglos.js
@@ -0,0 +1,23 @@
+/* Arreglos */
+let numerosArreglo = [];
+
+// Modificar los valores del arreglo en cada posición
+numerosArreglo[0] = 20;
+numerosArreglo[1] = 25;
+numerosArreglo[2] = 30;
+numerosArreglo[3] = 'Hola';
+numerosArreglo[4] = true;
+
+// Leer los valores
+console.log(`Elemento 1 - [0]: ${numerosArreglo[0]}`);
+console.log(`Elemento 2 - [1]: ${numerosArreglo[1]}`);
+console.log(`Elemento 3 - [2]: ${numerosArreglo[2]}`);
+console.log(`Elemento 4 - [3]: ${numerosArreglo[3]}`);
+console.log(`Elemento 5 - [4]: ${numerosArreglo[4]}`);
+
+let arregloNuevo = [1,2,3,4,5,'ocho','nueve'];
+
+console.log(`Elemento 1 - [0]: ${arregloNuevo[0]}`);
+console.log(`Elemento 2 - [1]: ${arregloNuevo[1]}`);
+console.log(`Elemento 6 - [4]: ${arregloNuevo[5]}`);
+console.log(`Elemento 7 - [1]: ${arregloNuevo[6]}`);
