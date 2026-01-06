5ec2dd086ad7da0492e5d1acbc9014b8370513fc
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-05 22:14:29 -0500

    Creando un ejemplo de matrices y accediendo a los valores de acuerdo a una posición dada para imprimir un valor

---
+++ matrices/matrices.js
@@ -0,0 +1,19 @@
+/* Matrices */
+
+// declaración
+let matriz = [[],[]];
+
+// modificar los valores de la matriz
+// reglon 0
+matriz[0][0] = 100;
+matriz[0][1] = 200;
+matriz[0][2] = 300;
+
+// reglon 1
+matriz[1][0] = 400;
+matriz[1][1] = 500;
+matriz[1][2] = 600;
+
+// leer los valores
+console.log(`Elemento [0][1] = ${matriz[0][1]}`);
+console.log(`Elemento [1][2] = ${matriz[1][2]}`);
