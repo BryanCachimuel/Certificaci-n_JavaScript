870ac14296a2158b6cfd1b737070381cdefcc32d
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-06 22:25:40 -0500

    Realizando paso a paso la creación de las funciones con retorno y como se realiza su proceso de impresión

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -7,3 +7,16 @@ function saludar(mensaje) {
 
 // 2. Llamar al procedimiento
 saludar('Hola');
+
+// declarar la función
+function sumar(a, b) {
+    let resultado = a + b;
+    return resultado;
+}
+
+// llamar o invocar a la función
+let argA = 8, argB = 9;
+let sumarResultado = sumar(argA,argB);
+
+// Imprimir el resultado de la suma
+console.log(`Resultado de la suma: ${sumarResultado}`);
