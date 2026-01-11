61c6ecce14fc73bb97c4237f3339f03e948cd65d
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-10 21:59:54 -0500

    Creando objetos y como acceder a las propiedad de los objetos

---
+++ objetos/objetos.js
@@ -0,0 +1,9 @@
+/* Objetos */
+let persona = {
+    nombre: 'Nelson',
+    apellido: 'Cacoango',
+    email: 'nelsonccc@gmail.com',
+    edad: 33
+}
+
+console.log(persona.nombre + " " + persona.apellido);
