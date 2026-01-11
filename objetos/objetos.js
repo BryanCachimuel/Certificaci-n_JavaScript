4e84cb4a3bc5543a17ea7fe707af175d355dfcb6
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-10 22:03:58 -0500

    Creando métodos dentro de los objetos y como imprimirlos

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -3,7 +3,14 @@ let persona = {
     nombre: 'Nelson',
     apellido: 'Cacoango',
     email: 'nelsonccc@gmail.com',
-    edad: 33
+    edad: 33,
+    nombreCompleto: function() {
+        return this.nombre + " " + this.apellido
+    }
 }
 
 console.log(persona.nombre + " " + persona.apellido);
+
+// impresión de la función creada dentro del objeto
+let datos = persona.nombreCompleto();
+console.log(datos);
