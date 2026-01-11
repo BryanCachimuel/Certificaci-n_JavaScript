3b680d83c5f44048f7abb7757561eae1926503c1
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-10 22:09:56 -0500

    Aprendiendo a crear objetos mediante la utilización de la palabra reservada new Object

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -14,3 +14,11 @@ console.log(persona.nombre + " " + persona.apellido);
 // impresión de la función creada dentro del objeto
 let datos = persona.nombreCompleto();
 console.log(datos);
+
+// Creación de objetos (poco comun)
+let persona2 = new Object();
+persona2.nombre = 'Kevin';
+persona2.apellido = 'Rodríguez';
+persona2.telefono = '0954834032';
+
+console.log(persona2.telefono);
