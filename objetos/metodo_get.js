bd6f054265ffd269897e9a7f804db201c2ecb329
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-17 21:54:34 -0500

    Creando un método set para poder cambiar los valores de una variable

--- objetos/metodo_get.js
+++ objetos/metodo_get.js
@@ -9,11 +9,18 @@ let persona = {
         return this.idioma.toUpperCase();
     },
 
+    set lang(lang) {
+        this.idioma = lang.toUpperCase()
+    },
+
     get nombreCompleto() {
         return this.nombre + " " + this.apellido
     }
 }
 
 console.log(persona.nombreCompleto)
+console.log(persona.lang);
+
+persona.lang = 'en';
 console.log(persona.lang);
 
