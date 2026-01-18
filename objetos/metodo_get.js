f9eb3f006a3a028ea9a5ee2ee4dcc8ec1c78d08d
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-17 21:51:40 -0500

    Creando un método get para tranformar el tipo de idioma de minusculas a mayusculas

--- objetos/metodo_get.js
+++ objetos/metodo_get.js
@@ -4,6 +4,10 @@ let persona = {
     apellido: 'Cacoango',
     email: 'nelsonccc@gmail.com',
     edad: 33,
+    idioma: 'es',
+    get lang() {
+        return this.idioma.toUpperCase();
+    },
 
     get nombreCompleto() {
         return this.nombre + " " + this.apellido
@@ -11,4 +15,5 @@ let persona = {
 }
 
 console.log(persona.nombreCompleto)
+console.log(persona.lang);
 
