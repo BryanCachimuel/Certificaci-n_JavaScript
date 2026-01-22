ad80e01add4a60d6b7c3a0759d3c298b8052bf4d
BryanCachimuel <bryanloyo56@gmail.com>
2026-01-21 22:41:28 -0500

    Aprendiendo a crear objetos en diferentes formas

--- objetos/metodo_constructor.js
+++ objetos/metodo_constructor.js
@@ -17,3 +17,22 @@ console.log(madre);
 
 padre.nombre = 'Camilo';
 console.log(padre.nombrecompleto());
+
+/* Distintas formas de declarar objetos */
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+
+let miBoolean1 = new Boolean(false);
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+let miArreglo2 = [];
+
+let miFuncion = new Function();
+let miFuncion2 = function(){};
